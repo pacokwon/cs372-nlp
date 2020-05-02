aee6d7dd9413fc98ea36345a46a841a6235a9f05
pacokwon <haechank@gmail.com>
2020-05-02 14:42:48 +0900

    update(hw2): comments to each function

--- hw2/hw2.py
+++ hw2/hw2.py
@@ -3,7 +3,7 @@
 
 import nltk
 from pprint import pprint
-from nltk.corpus import brown, masc_tagged, wordnet, gutenberg, reuters
+from nltk.corpus import brown, masc_tagged, wordnet, gutenberg, reuters, inaugural
 from nltk.corpus import stopwords
 from functools import reduce
 from nltk.sentiment.vader import SentimentIntensityAnalyzer
@@ -11,6 +11,10 @@ import pickle
 
 
 class POSCategorizer:
+    """
+    A collection of methods that classifies the pos of words
+    """
+
     @staticmethod
     def is_adverb(pos):
         return pos in ["RB", "RBR", "RBT"]
@@ -29,6 +33,19 @@ class POSCategorizer:
 
     @classmethod
     def is_qualified_pos_pair(cls, word1, word2):
+        """
+        Returns whether or not the two word's POS sequence is what we define as 'qualified'
+
+        'Qualified' is defined as a certain pattern of two pos's that this method defines
+        the local variable pos_pair defines such patterns with the class's static methods
+
+        :param word1: first word bigram
+        :type word1: a tuple of (token, pos)
+        :param word2: second word bigram
+        :type word2: a tuple of (token, pos)
+        :returns: True if the two word's POS sequence is 'qualified', False otherwise
+        :rtype: boolean
+        """
         pos1 = word1[1]
         pos2 = word2[1]
 
@@ -63,34 +80,48 @@ def filtered_bigrams(tagged_words, tagger):
 
 def get_tagged_words():
     """
-    Returns all alphabetical words from a given corpus except stopwords
+    Returns all alphabetical words from the brown and masc_tagged corpora
 
-    :returns: a list of alphabetical words that are not stopwords
+    :returns: a list of words
     :rtype: a list
     """
-    english_stopwords = stopwords.words("english")
     return (
         (word[0].lower(), word[1])
-        for word in brown.tagged_words() + masc_tagged.tagged_words() + tagger.tag
-        if word[0].isalpha() and word[0] not in english_stopwords
+        for word in brown.tagged_words() + masc_tagged.tagged_words()
     )
 
 
 def generate_scores(cfd, limit):
+    """
+    Returns a dictionary that contains a bigram as the key, and the score of the bigram as the value
+
+    :param cfd: ConditionalFreqDist object that contains the occurrences of bigrams
+    :type cfd: ConditionalFreqDist object
+    :param limit: the maximum number(exclusive) of modified words for a modifier
+    :type limit: int
+    """
     sid = SentimentIntensityAnalyzer()
+
+    def polarity_score(cond, mod):
+        combined = sid.polarity_scores(f"{cond} {mod}")
+        single = sid.polarity_scores(mod)
+
+        return abs(
+            combined["compound"] + combined["pos"] - single["compound"] + single["pos"]
+        )
+
+    def increased(cond, mod):
+        combined = sid.polarity_scores(f"{cond} {mod}")["compound"]
+        single = sid.polarity_scores(mod)["compound"]
+        return combined > single and combined * single >= 0
+
     return {
         (condition, modified): {
             "score": cfd[condition][modified]
             / len(cfd[condition])
-            * abs(
-                sid.polarity_scores(f"{condition} {modified}")["compound"]
-                - sid.polarity_scores(modified)["compound"]
-            ),
-            "frac": f"{cfd[condition][modified]} / {len(cfd[condition])}",
-            "sentiment_before": sid.polarity_scores(modified),
-            "sentiment_after": sid.polarity_scores(f"{condition} {modified}"),
-            "increased": sid.polarity_scores(f"{condition} {modified}")["compound"]
-            > sid.polarity_scores(modified)["compound"],
+            * polarity_score(condition, modified)
+            * 100,
+            "increased": increased(condition, modified),
         }
         for condition in cfd.keys()
         for modified in cfd[condition]
@@ -99,11 +130,17 @@ def generate_scores(cfd, limit):
 
 
 def get_tagger():
+    """
+    Return a POS tagger; generate one if it doesn't exist
+
+    :returns: nlkt.BigramTagger object trained with brown corpora
+    :rtype: nltk.BigramTagger object
+    """
     try:
         with open("tagger.pkl", "rb") as file:
             return pickle.load(file)
     except (FileNotFoundError, EOFError):
-        sents = brown.tagged_sents()
+        sents = brown.tagged_sents() + masc_tagged.tagged_sents()
         t0 = nltk.DefaultTagger("NN")
         t1 = nltk.UnigramTagger(sents, backoff=t0)
         t2 = nltk.BigramTagger(sents, backoff=t1)
@@ -121,30 +158,55 @@ def add_corpus_entries(cfd, *corpora):
     :param *corpora: nltk.corpus objects
     :type *corpora: a list of nltk.corpus objects
     """
-    words = reduce(lambda x, y: x + y, corpora, [])
+    words = reduce(lambda acc, cur: acc + cur.words(), corpora, [])
     for bigram in nltk.bigrams(words):
         if bigram[0] in cfd.keys() and bigram[1] in cfd[bigram[0]]:
-            # print(bigram, cfd[bigram[0]][bigram[1]])
             cfd[bigram[0]][bigram[1]] += 1
-            # print(bigram, cfd[bigram[0]][bigram[1]])
 
 
-if __name__ == "__main__":
-    tagger = get_tagger()
-    tagged_words = get_tagged_words()
+def write_to_csv(filename, pairs):
+    """
+    Save pairs into a csv format file
+
+    :param filename: expected file name of csv file
+    :type filename: string
+    :param pairs: list of bigrams
+    :type pairs: list of tuples of strings
+    """
+    with open(filename, "w") as f:
+        print("\n".join(f"{pair[0]},{pair[1]}" for pair in pairs), file=f)
+
+
+def main():
+    """
+    main function of program
+    """
+    tagger = get_tagger()  # retrieve trained tagger
+    tagged_words = get_tagged_words()  # retrieve tagged words list
+
+    # filter bigrams of tagged words by pos
     bigrams = filtered_bigrams(tagged_words, tagger)
 
+    # construct a ConditionalFreqDist
     cfd = nltk.ConditionalFreqDist(bigrams)
-    # add_corpus_entries(cfd, gutenberg, reuters)
 
-    score_pairs = generate_scores(cfd, 30)
+    # collect additional text info from other corpora
+    add_corpus_entries(cfd, gutenberg, reuters, inaugural)
+
+    # grade each pairs
+    score_pairs = generate_scores(cfd, 9)
+
     filtered = {
         key: score_pairs[key] for key in score_pairs if score_pairs[key]["increased"]
     }
+
     sorted_pairs = [
-        (k, v["score"])
+        k
         for k, v in sorted(filtered.items(), key=lambda x: x[1]["score"], reverse=True)
     ]
+    pprint(sorted_pairs[:100])
+    write_to_csv("result.csv", sorted_pairs)
 
-    for idx, pair in enumerate(sorted_pairs):
-        print(idx, pair)
+
+if __name__ == "__main__":
+    main()
